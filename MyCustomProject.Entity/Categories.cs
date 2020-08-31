aa7a7c0ac11debeb83fc9ff0674005770c1eec50
Rahib Jafar <rahib.cefer.98@gmail.com>
2020-08-31 15:50:14 +0400

    first step

---
+++ MyCustomProject.Entity/Categories.cs
@@ -0,0 +1,18 @@
+﻿using MyCustomProject.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCustomProject.Entity
+{
+    [Table(TableName = "Categories", IdentityColumn = "CategoryID",IsActiveColumn = "IsActive",PrimaryColumn = "CategoryID",ForeignColumn ="blabla")]
+    public class Categories
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ MyCustomProject.ORM/CategoryORM.cs
@@ -0,0 +1,15 @@
+﻿using MyCustomProject.Common;
+using MyCustomProject.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCustomProject.ORM
+{
+    public class CategoryORM : ORMBase<Categories,CategoryORM>
+    {
+
+    }
+}
